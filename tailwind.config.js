87caa95c521802844cc2500b65d27c944e2cd1a1
Sidhartha D <144550797+dsid271@users.noreply.github.com>
2024-12-23 00:25:19 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
     extend: {
       fontFamily: {
         extrude: ['Extrude', 'sans-serif'],
-        commodore: ['Commodore', 'sans-serif'],
+        commodore: ['commodore', 'sans-serif'],
       },
     },
   },
