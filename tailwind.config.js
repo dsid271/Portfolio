26a433f68f8bd90278ee1e72ae994ecfa964cd5e
Sidhartha D <144550797+dsid271@users.noreply.github.com>
2024-12-23 00:48:34 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',  // Ensure this is correct
+    './components/**/*.{js,ts,jsx,tsx}',  // Ensure this is correct
+    './src/**/*.{js,ts,jsx,tsx}',
+    './**/*.{html}',  // Add this if you're using .html files too
+  ],
   theme: {
     extend: {
       fontFamily: {
-        extrude: ['Extrude', 'sans-serif'],
-        commodore: ['commodore', 'sans-serif'],
-      },
+        extrude: ['extrude', 'sans-serif'],
+        commodore: ['commodore', 'monospace'],      },
     },
   },
   plugins: [],
