fde28d17a00b021bf2dae192632aaa3e1aaf5cbc
Sidhartha D <144550797+dsid271@users.noreply.github.com>
2024-12-23 21:28:18 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bright-pink': '#FF007F',
+        'bright-yellow': '#FFD700',
+        'bright-green': '#00FF00',
+      },
+    },
   },
   plugins: [],
 };
